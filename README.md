e21bece6f31fef5f7c624261dab210f56df7a018
HeleneElena <heleneebert1@gmail.com>
2022-09-16 09:10:10 +0200

    commitGame

---
+++ README.md
@@ -0,0 +1,8 @@
+# Errate die Zahl Spiel React
+Der Benutzer muss eine Zufallszahl zwischen 1 und 10 erraten. Wenn sie eine Zahl eingeben, die niedriger oder höher als die Zufallszahl ist, werden sie dazu aufgefordert, diese einzugeben. Sie sehen auch, wie viele Versuche sie noch haben (insgesamt 6 Versuche).
+
+Игра "Угадай число" React
+Пользователь должен угадать рандомное загаданное число от 1 до 10. Если он ввел число меньше загаданного или больше загаданного, он видит соответствующее сообщение. Также он видит сколько попыток у него осталось (всего 6 попыток).
+
+Guess the Number Game React
+The user must guess a random number between 1 and 10. If he/she enters a number less than or greater than the number he/she guessed, he/she sees a corresponding message. He also sees how many tries he has left (a total of 6 tries).
---
+++ src/components/ClassComponent/ClassComponent.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from './ClassComponent.module.css';
+
+export class ClassComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+          userNumber: '',
+          randomNumber:
+            Math.floor(Math.random() * (this.props.max - this.props.min) + this.props.min),
+          result: `Загадано число от ${this.props.min} до ${this.props.max}. У вас 6 попыток!`,
+          count: 0,
+          isStatus: false,
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      userNumber: e.target.value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState((state => {
+      if (state.isStatus) {
+        return {
+          userNumber: '',
+          randomNumber:
+            Math.floor(Math.random() * (this.props.max - this.props.min) + this.props.min),
+          result: `Загадано число от ${this.props.min} до ${this.props.max}. У вас 6 попыток!`,
+          count: 0,
+          isStatus: false,
+        };
+      }
+    }));
+  
+    this.setState(state => {
+      if (state.userNumber.trim() === '' || 
+        !(state.userNumber >= this.props.min && state.userNumber <= this.props.max)) {
+        return {
+          count: state.count,
+        };
+      }
+
+      if (state.userNumber > state.randomNumber && state.count < 5) {
+        return {
+          result: `${state.userNumber} больше, чем загаданное, у вас осталось ${5 - state.count} попыток`,
+          userNumber: '',
+          count: state.count + 1,
+        };
+      }
+
+      if (state.userNumber < state.randomNumber && state.count < 5) {
+        return {
+          result: `${state.userNumber} меньше, чем загаданное, у вас осталось ${5 - state.count} попыток`,
+          userNumber: '',
+          count: state.count + 1,
+        };
+      }
+
+      if (state.count === 5) {
+        return {
+          result: `У вас закончились попытки. Попробуйте еще!`,
+          userNumber: '',
+          count: state.count,
+          isStatus: true,
+        };
+      }
+
+      return {
+        result: `Поздравляем, вы угадали! Это ${state.userNumber},
+                  попыток ${state.count}`,
+        userNumber: '',
+        isStatus: true,
+      };
+    }); 
+  };
+
+  render() {
+    return (
+      <div className={style.game}>
+        <p className={style.result}>{this.state.result}</p>
+
+        <form className={style.form} onSubmit={this.handleSubmit} >
+          <label className={style.label} htmlFor='user_number'>
+            Введите число
+          </label>
+
+          <input onChange={this.handleChange} 
+            value={this.state.userNumber}
+            disabled={this.state.isStatus}
+            className={style.input} type='number' id='user_number'>
+          </input>
+
+          <button className={style.btn}>
+            {this.state.isStatus ? 'Сыграть ещё' : 'Угадай'}
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+ClassComponent.propTypes = {
+  min: PropTypes.number,
+  max: PropTypes.number,
+};
